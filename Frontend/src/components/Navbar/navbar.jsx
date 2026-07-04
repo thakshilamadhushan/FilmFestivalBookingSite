bf69f6716a362a6e3adfcb11fb9e62a396a07346
Thakshila Madhushan <thakshilamadushan900@gmail.com>
2026-07-04 22:01:58 +0530

    Make React project and Create Nav Bar

---
+++ Frontend/src/components/Navbar/navbar.jsx
@@ -0,0 +1,42 @@
+import "./navbar.css";
+import { FaTicketAlt, FaFilm, FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+
+        <div className="logoIcon">
+          <FaFilm />
+        </div>
+
+        <div>
+          <h2>FIHST Film Festival</h2>
+          <span>2026 EDITION</span>
+        </div>
+
+      </div>
+
+      <ul className={menuOpen ? "navLinks active" : "navLinks"}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Movies</a></li>
+        <li><a href="#">Download Ticket</a></li>
+        <li><a href="#">About</a></li>
+      </ul>
+
+      <div
+        className="menu"
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        {menuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
